6f0104c1b50642d281623e82f895f5acaa088536
Lucas Sodré <lucassodrem@gmail.com>
2021-03-29 08:30:23 -0300

    Heroku db config.

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'easy_personal',
-  password: 'admin123',
+  user: 'boqcuwzvvkvqqi',
+  host: 'ec2-54-164-22-242.compute-1.amazonaws.com',
+  database: 'd26t916m12anua',
+  password: '03b40d13599d203ed3972c6f32815218735e8ca2658f8ab069fb189d7c285efb',
   port: 5432,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool
