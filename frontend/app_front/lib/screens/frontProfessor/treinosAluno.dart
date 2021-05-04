aef987bf2c745a7bbca037b01616bc2d7cbbf6dc
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-05-03 23:52:42 -0300

    [feat]: add da lista de treinos por aluno e barra de pesquisa de treinos (não funcional)

--- frontend/app_front/lib/screens/frontProfessor/treinosAluno.dart
+++ frontend/app_front/lib/screens/frontProfessor/treinosAluno.dart
@@ -16,137 +16,137 @@ class _TreinosAlunoState extends State<TreinosAluno> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(right: 10, left: 10),
-            child: Container(
-              padding: EdgeInsets.all(10),
-              height: 180,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 3,
-                    blurRadius: 4,
-                    offset:
-                    Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  stops: [0.1, 0.8],
-                  colors: [
-                    Color(0xFF3594DD),
-                    Color(0xFF4563DB),
-                  ],
-                ),
-              ),
-              child: Padding(
-                padding: EdgeInsets.only(top: 14),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          width: 170,
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(
-                            "Treino Boladão",
-                            style: TextStyle(
-                              fontSize: 22.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.only(right: 18),
-                          child: Text(
-                            "Ativo",
-                            style: TextStyle(
-                                fontSize: 20.0
-                            ),
-                          ),
-                        ),
-
-                      ],
-                    ),
-                    SizedBox(
-                      height: 17.0,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(
-                            "29/04/2021 a 29/05/2021",
-                            style: TextStyle(
-                                fontSize: 18.0
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 17.0,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.only(left: 10),
-                          child: ElevatedButton(
-                            onPressed: (){},
-                            child: Text(
-                              "Ver treino",
-                              style: TextStyle(
-                                  fontSize: 18.0
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-
-
-                  ],
-
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 100.0,
-          ),
-          Row(
-            children: [
-              ElevatedButton(
-                onPressed: (){
-                  print("[DEBUG ID]: ${widget.instructorId}");
-                  Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                        builder: (context) => PageMainTeacher(instructorId: widget.instructorId)
-                    ),
-                  );
-                },
-                child: Text(
-                  "VOLTAR",
-                  style: TextStyle(
-                      fontSize: 18.0
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
+      appBar: AppBar(
+        title: Text( "Easy Personal"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildContainerPesquisa(),
+            _buildListaTreinos("Treino de aquecimento", "03/05/2021", "03/06/2021", "ATIVO", Colors.green),
+            _buildListaTreinos("Treino de introdução", "03/04/2021", "03/05/2021", "INATIVO", Colors.red),
+            _buildListaTreinos("Treino teste", "25/03/2021", "01/04/2021", "INATIVO", Colors.red),
+          ],
+        ),
       ),
     );
   }
+
+  Widget _buildContainerPesquisa() => Container(
+    color: Colors.blue,
+    width: MediaQuery.of(context).size.width,
+    padding: EdgeInsets.only(top: 20, right: 10, bottom: 20, left: 10),
+    child: TextField(
+      decoration: InputDecoration(
+        suffixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        fillColor: Colors.white,
+        filled: true,
+        labelText: 'Pesquisar treino',
+      ),
+    ),
+  );
+
+  Widget _buildListaTreinos(String nomeTreino, String dataInicio, String dataFim, String status, Color color) => Padding(
+    padding: EdgeInsets.only(right: 10, left: 10, top: 20),
+    child: Container(
+      padding: EdgeInsets.only(bottom: 15, left: 10, top: 5),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 4,
+            offset:
+            Offset(0, 3), // changes position of shadow
+          ),
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(top: 14),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 270,
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    nomeTreino,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.only(right: 18),
+                  child: Chip(
+                    labelPadding: EdgeInsets.all(2.0),
+                    label: Text(
+                      status,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    backgroundColor: color,
+                    elevation: 6.0,
+                    shadowColor: Colors.grey[60],
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 17.0,
+            ),
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "${dataInicio} a ${dataFim}",
+                    style: TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.blueGrey,
+
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 17.0,
+            ),
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 8),
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    child: Text(
+                      "Ver treino",
+                      style: TextStyle(
+                          fontSize: 18.0
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+
 }
