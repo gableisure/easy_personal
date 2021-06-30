c407f7a3785f227672a2dd2f8a57d49925af492a
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-06-30 00:10:47 -0300

    add tela de lista de treinos do aluno

--- frontend/app_front/lib/api/apiGetStudentTrainings.dart
+++ frontend/app_front/lib/api/apiGetStudentTrainings.dart
@@ -2,6 +2,7 @@ import "dart:async";
 import 'dart:convert';
 import 'package:app_front/models/studentTraning.dart';
 import 'package:http/http.dart' as http;
+import 'package:app_front/helpers/globals.dart' as globals;
 
 class APIGetStudentTrainings {
   Future<StudentTraining> getAllTrainings() async {
@@ -10,14 +11,17 @@ class APIGetStudentTrainings {
     String route = "/api/v1/usuarios/trainings/";
 
     final http.Response response = await http.get(Uri.http(url, route), headers: <String, String>{
-      "content-type": "application/json; charset=UTF-8"
+      "content-type": "application/json; charset=UTF-8",
+      "Cookie": globals.rawCookieStudent
     });
 
     if(response.statusCode == 200) {
       print("Deu certo");
+      print(json.decode(response.body));
       return StudentTraining.fromJson(json.decode(response.body));
     } else if(response.statusCode == 401){
       print("Deu errado");
+      print(json.decode(response.body));
       return StudentTraining.fromJson(json.decode(response.body));
     }
     print("Deu nada");
