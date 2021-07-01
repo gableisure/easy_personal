fd0d934b76a5138cffb6ff19fda2deb986f71300
iagoNogueira <iagonogueira500@gmail.com>
2021-07-01 18:58:42 -0300

    add da função de Deletar exercício

---
+++ frontend/app_front/lib/widgets/alertDeleteExercise.dart
@@ -0,0 +1,61 @@
+import 'package:app_front/screens/frontProfessor/pageMainTeacher.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AlertDeleteExercise extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(25.0),
+        ),
+      ),
+      titlePadding: EdgeInsets.only(top: 20, left: 30, right: 30),
+      title: Text(
+        "Exercício deletado com sucesso!",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w500,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      contentPadding: EdgeInsets.only(top: 15, left: 30, right: 30),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Icon(
+                Icons.check,
+                size: 100.0,
+                color: Colors.green
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) => PageMainTeacher(),
+                        ));
+
+                  },
+                  child: Text(
+                    "Continuar",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                      color: Colors.blueAccent,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
